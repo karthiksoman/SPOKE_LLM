69463a21fc253b4c69c5d508234877b5ed705074
Karthik Soman <karthi.soman@gmail.com>
2023-11-14 13:29:44 -0800

    added both rag and prompt based for demo

--- codes/py_scripts/rag_based_text_generation/GPT/demonstration.py
+++ codes/py_scripts/rag_based_text_generation/GPT/demonstration.py
@@ -3,6 +3,8 @@ sys.path.insert(0, "../../")
 from utility import *
 
 
+response_type = sys.argv[1]
+
 CONTEXT_VOLUME = 150
 QUESTION_VS_CONTEXT_SIMILARITY_PERCENTILE_THRESHOLD = 93
 QUESTION_VS_CONTEXT_MINIMUM_SIMILARITY = 0.5
@@ -30,62 +32,78 @@ node_context_df = pd.read_csv(NODE_CONTEXT_PATH)
 question = input("Enter your question : ") 
 print(" ")
 
-input("Press enter for Step 1 - Disease entity extraction using GPT-3.5-Turbo")
-print("Processing ...")
-entities = disease_entity_extractor(question)
-max_number_of_high_similarity_context_per_node = int(CONTEXT_VOLUME/len(entities))
-print("Extracted entity from the prompt = '{}'".format(", ".join(entities)))
-print(" ")
-
-input("Press enter for Step 2 - Match extracted Disease entity to SPOKE nodes")
-print("Finding vector similarity ...")
-node_hits = []
-for entity in entities:
-    node_search_result = vectorstore.similarity_search_with_score(entity, k=1)
-    node_hits.append(node_search_result[0][0].page_content)
-print("Matched entities from SPOKE = '{}'".format(", ".join(node_hits)))
-print(" ")
-
-input("Press enter for Step 3 - Context extraction from SPOKE")
-node_context = []
-for node_name in node_hits:
-    node_context.append(node_context_df[node_context_df.node_name == node_name].node_context.values[0])
-print("Extracted Context is : ")
-print(". ".join(node_context))
-print(" ")
-
-input("Press enter for Step 4 - Context pruning")
-question_embedding = embedding_function_for_context_retrieval.embed_query(question)
-node_context_extracted = ""
-for node_name in node_hits:
-    node_context = node_context_df[node_context_df.node_name == node_name].node_context.values[0]
-    node_context_list = node_context.split(". ")        
-    node_context_embeddings = embedding_function_for_context_retrieval.embed_documents(node_context_list)
-    similarities = [cosine_similarity(np.array(question_embedding).reshape(1, -1), np.array(node_context_embedding).reshape(1, -1)) for node_context_embedding in node_context_embeddings]
-    similarities = sorted([(e, i) for i, e in enumerate(similarities)], reverse=True)
-    percentile_threshold = np.percentile([s[0] for s in similarities], QUESTION_VS_CONTEXT_SIMILARITY_PERCENTILE_THRESHOLD)
-    high_similarity_indices = [s[1] for s in similarities if s[0] > percentile_threshold and s[0] > QUESTION_VS_CONTEXT_MINIMUM_SIMILARITY]
-    if len(high_similarity_indices) > max_number_of_high_similarity_context_per_node:
-        high_similarity_indices = high_similarity_indices[:max_number_of_high_similarity_context_per_node]
-    high_similarity_context = [node_context_list[index] for index in high_similarity_indices]
-    node_context_extracted += ". ".join(high_similarity_context)
-    node_context_extracted += ". "
-print("Pruned Context is : ")
-print(node_context_extracted)
-print(" ")
-
-input("Press enter for Step 5 - LLM prompting")
-enriched_prompt = "Context: "+ node_context_extracted + "\n" + "Question: " + question
-print("Here is the enriched input to the LLM :")
-print(enriched_prompt)
-print(" ")
-
-input("Press enter for Step 6 - LLM response")
-print("Calling GPT ...")
-output = get_GPT_response(enriched_prompt, SYSTEM_PROMPT, CHAT_MODEL_ID, CHAT_DEPLOYMENT_ID, temperature=TEMPERATURE)
-print(output)
-print(" ")
+def main():
+    if response_type == "rag":
+        rag_demo(question)
+    else:
+        prompt_demo(question)
+        
+        
+def prompt_demo(question):
+    print("Calling GPT ...")
+    output = get_GPT_response(question, SYSTEM_PROMPT, CHAT_MODEL_ID, CHAT_DEPLOYMENT_ID, temperature=TEMPERATURE)
+    print(output)
+    print("")
 
 
+def rag_demo(question):
+    input("Press enter for Step 1 - Disease entity extraction using GPT-3.5-Turbo")
+    print("Processing ...")
+    entities = disease_entity_extractor(question)
+    max_number_of_high_similarity_context_per_node = int(CONTEXT_VOLUME/len(entities))
+    print("Extracted entity from the prompt = '{}'".format(", ".join(entities)))
+    print(" ")
+
+    input("Press enter for Step 2 - Match extracted Disease entity to SPOKE nodes")
+    print("Finding vector similarity ...")
+    node_hits = []
+    for entity in entities:
+        node_search_result = vectorstore.similarity_search_with_score(entity, k=1)
+        node_hits.append(node_search_result[0][0].page_content)
+    print("Matched entities from SPOKE = '{}'".format(", ".join(node_hits)))
+    print(" ")
+
+    input("Press enter for Step 3 - Context extraction from SPOKE")
+    node_context = []
+    for node_name in node_hits:
+        node_context.append(node_context_df[node_context_df.node_name == node_name].node_context.values[0])
+    print("Extracted Context is : ")
+    print(". ".join(node_context))
+    print(" ")
+
+    input("Press enter for Step 4 - Context pruning")
+    question_embedding = embedding_function_for_context_retrieval.embed_query(question)
+    node_context_extracted = ""
+    for node_name in node_hits:
+        node_context = node_context_df[node_context_df.node_name == node_name].node_context.values[0]
+        node_context_list = node_context.split(". ")        
+        node_context_embeddings = embedding_function_for_context_retrieval.embed_documents(node_context_list)
+        similarities = [cosine_similarity(np.array(question_embedding).reshape(1, -1), np.array(node_context_embedding).reshape(1, -1)) for node_context_embedding in node_context_embeddings]
+        similarities = sorted([(e, i) for i, e in enumerate(similarities)], reverse=True)
+        percentile_threshold = np.percentile([s[0] for s in similarities], QUESTION_VS_CONTEXT_SIMILARITY_PERCENTILE_THRESHOLD)
+        high_similarity_indices = [s[1] for s in similarities if s[0] > percentile_threshold and s[0] > QUESTION_VS_CONTEXT_MINIMUM_SIMILARITY]
+        if len(high_similarity_indices) > max_number_of_high_similarity_context_per_node:
+            high_similarity_indices = high_similarity_indices[:max_number_of_high_similarity_context_per_node]
+        high_similarity_context = [node_context_list[index] for index in high_similarity_indices]
+        node_context_extracted += ". ".join(high_similarity_context)
+        node_context_extracted += ". "
+    print("Pruned Context is : ")
+    print(node_context_extracted)
+    print(" ")
+
+    input("Press enter for Step 5 - LLM prompting")
+    enriched_prompt = "Context: "+ node_context_extracted + "\n" + "Question: " + question
+    print("Here is the enriched input to the LLM :")
+    print(enriched_prompt)
+    print(" ")
+
+    input("Press enter for Step 6 - LLM response")
+    print("Calling GPT ...")
+    output = get_GPT_response(enriched_prompt, SYSTEM_PROMPT, CHAT_MODEL_ID, CHAT_DEPLOYMENT_ID, temperature=TEMPERATURE)
+    print(output)
+    print(" ")
 
 
+
+if __name__ == "__main__":
+    main()
